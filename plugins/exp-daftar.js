569b1888f5190f91cb40f21ced714a4a13e7b145
izaii <koejelek12345@gmail.com>
2022-09-18 20:11:17 +0700

    Update exp-daftar.js

--- plugins/exp-daftar.js
+++ plugins/exp-daftar.js
@@ -11,39 +11,39 @@ let handler = async function (m, { text, usedPrefix, command }) {
 	{
 	title: "Select Your Age Here !",
 	rows: [
-	    {title: "Random Years", rowId: '.daftar ' + namae + '.' + pickRandom(['30','29','28','27','26','25','24','23','22','21','20','19','18','17','16','15','14','13','12','11','10','9'])}
+	    {title: "Random Years", rowId: '.reg ' + namae + '.' + pickRandom(['30','29','28','27','26','25','24','23','22','21','20','19','18','17','16','15','14','13','12','11','10','9'])}
 	]
     },
     {
 	title: "O L D",
 	rows: [
-	    {title: "30 Years", rowId: '.daftar ' + namae + '.30 '},
-	    {title: "29 Years", rowId: '.daftar ' + namae + '.29 '},
-	    {title: "28 Years", rowId: '.daftar ' + namae + '.28 '},
-	{title: "27 Years", rowId: '.daftar ' + namae + '.27 '},
-	{title: "26 Years", rowId: '.daftar ' + namae + '.26 '},
-	{title: "25 Years", rowId: '.daftar ' + namae + '.25 '},
-	{title: "24 Years", rowId: '.daftar ' + namae + '.24 '},
-	{title: "23 Years", rowId: '.daftar ' + namae + '.23 '},
-	{title: "22 Years", rowId: '.daftar ' + namae + '.22 '},
-	{title: "21 Years", rowId: '.daftar ' + namae + '.21 '}
+	    {title: "30 Years", rowId: '.reg ' + namae + '.30 '},
+	    {title: "29 Years", rowId: '.reg ' + namae + '.29 '},
+	    {title: "28 Years", rowId: '.reg ' + namae + '.28 '},
+	{title: "27 Years", rowId: '.reg ' + namae + '.27 '},
+	{title: "26 Years", rowId: '.reg ' + namae + '.26 '},
+	{title: "25 Years", rowId: '.reg ' + namae + '.25 '},
+	{title: "24 Years", rowId: '.reg ' + namae + '.24 '},
+	{title: "23 Years", rowId: '.reg ' + namae + '.23 '},
+	{title: "22 Years", rowId: '.reg ' + namae + '.22 '},
+	{title: "21 Years", rowId: '.reg' + namae + '.21 '}
 	]
     },
     {
 	title: "Y O U N G",
 	rows: [
-	    {title: "20 Years", rowId: '.daftar ' + namae + '.20 '},
-	    {title: "19 Years", rowId: '.daftar ' + namae + '.19 '},
-	    {title: "18 Years", rowId: '.daftar ' + namae + '.18 '},
-	{title: "17 Years", rowId: '.daftar ' + namae + '.17 '},
-	{title: "16 Years", rowId: '.daftar ' + namae + '.16 '},
-	{title: "15 Years", rowId: '.daftar ' + namae + '.15 '},
-	{title: "14 Years", rowId: '.daftar ' + namae + '.14 '},
-	{title: "13 Years", rowId: '.daftar ' + namae + '.13 '},
-	{title: "12 Years", rowId: '.daftar ' + namae + '.12 '},
-	{title: "11 Years", rowId: '.daftar ' + namae + '.11 '},
-	{title: "10 Years", rowId: '.daftar ' + namae + '.10 '},
-	{title: "9 Years", rowId: '.daftar ' + namae + '.9 '}
+	    {title: "20 Years", rowId: '.reg ' + namae + '.20 '},
+	    {title: "19 Years", rowId: '.reg ' + namae + '.19 '},
+	    {title: "18 Years", rowId: '.reg ' + namae + '.18 '},
+	{title: "17 Years", rowId: '.reg ' + namae + '.17 '},
+	{title: "16 Years", rowId: '.reg ' + namae + '.16 '},
+	{title: "15 Years", rowId: '.reg ' + namae + '.15 '},
+	{title: "14 Years", rowId: '.reg ' + namae + '.14 '},
+	{title: "13 Years", rowId: '.reg ' + namae + '.13 '},
+	{title: "12 Years", rowId: '.reg ' + namae + '.12 '},
+	{title: "11 Years", rowId: '.reg ' + namae + '.11 '},
+	{title: "10 Years", rowId: '.reg ' + namae + '.10 '},
+	{title: "9 Years", rowId: '.reg ' + namae + '.9 '}
 	]
     },
 ]
